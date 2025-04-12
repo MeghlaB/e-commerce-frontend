72de6f5e450e4d8da2640a1255932640dc949a68
MeghlaB <meghlab964@gmail.com>
2025-04-12 19:25:53 +0600

    user-dashboard

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,24 +1,28 @@
+"use client";
 // app/dashboard/layout.tsx
 
-import Sidebar from "../components/ui/adminSidebar";
-import Topbar from "../components/ui/topper";
+
 
 
 
 export default function DashboardLayout({
   children,
+ 
 }: {
   children: React.ReactNode;
+ 
 }) {
+  
   return (
     <div>
-      <Sidebar />
-      <div className="md:ml-[250px]"> 
+  
+      
+      
         {/* the left margin remains always the same , so that in little screen it breaks the design
         . thats why i added md */}
-        <Topbar />
-        <main className="pt-20 px-6">{children}</main>
-      </div>
+      
+        <main >{children}</main>
+      
     </div>
   );
 }
